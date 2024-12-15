5ad4da1e5e764b968890db0543d966116e56455e
shivani-meena <110822540+shivani-meena@users.noreply.github.com>
2024-12-15 16:40:42 +0530

    Update insertion_sort.py

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -9,3 +9,14 @@ while i < len(arr):
     i += 1 
 print(arr)
 
+
+arr = [15, 4, 45, 13, 6, 2, 3, 8]
+i = 0
+while i <= len(arr)-1:
+    j = i
+    while j > 0 and arr [ j ] < arr [ j - 1 ]: #stops if the array is already sorted 
+        arr [ j - 1] , arr [ j ]  =  arr [ j ] , arr [ j - 1] 
+        j -= 1 
+    i += 1 
+print(arr)
+
